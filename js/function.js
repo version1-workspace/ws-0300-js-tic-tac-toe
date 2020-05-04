a3a42cfa3a09668d5b98698d07048321e6fe9b92
Kahoru Hirai <>
2020-05-04 17:19:48 +0900

    マルバツの切り替え入力とリセット機能

--- js/function.js
+++ js/function.js
@@ -1,21 +1,55 @@
-  let BoxElements = document.querySelectorAll('.js-box');
-  const Boxes = Object.keys(BoxElements);
-  //console.log(Boxes);
+let Boxes = document.querySelectorAll('.js-box');
 
-  const Marks = {
-    maru: '◯',
-    Batsu: '×'
-  }
-//console.log(Marks.maru);
+const PlayerMaru = document.querySelector('.maru');
+const PlayerBatsu = document.querySelector('.batsu');
+const messages = document.querySelector('.js-message');
+
+let Turn = 0;
+
+
+const Marks = {
+  maru: '◯',
+  batsu: '×'
+}
+
+/*
+const WinningPattern = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6]
+];
+*/
 
 function putMark(index) {
-  Boxes[index].innerHTML = Marks.Batsu;
+  Boxes[index].innerHTML =  Marks.maru;
+  Turn++;
+
+  if(Turn % 2 == 0) {
+    Boxes[index].innerHTML =  Marks.batsu;
+    PlayerMaru.classList.add('active');
+    PlayerBatsu.classList.remove('active');
+
+  } else {
+    Boxes[index].innerHTML =  Marks.maru;
+    PlayerBatsu.classList.add('active');
+    PlayerMaru.classList.remove('active');
+  }
 }
 
-for (let i=0; i < Boxes.length; i++) {
-  Boxes[i].addEventListener('click', function() {
-    console.log('途中');
-    putMark(index);
-  }, false);
-}
+window.onload = function() {
 
+  for (let i = 0; i < Boxes.length; i++) {
+    Boxes[i].addEventListener('click', function(e) {
+      if(e.target.innerHTML !== ""){
+        return
+      } 
+      putMark(i);
+    }, false);
+  }
+  document.getElementById('js-restart').addEventListener('click', () => location.reload());
+}
