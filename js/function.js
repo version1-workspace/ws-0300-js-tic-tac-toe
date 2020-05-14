bf369ab3e6d568ae7ced8273d5bdc0986b844100
Toma Otani <otani.toma@gmail.com>
2020-05-14 15:13:13 -0700

    add array for line

--- js/function.js
+++ js/function.js
@@ -1,18 +1,34 @@
 
-let turnFirst = false;
+let turnFirst = true;
 
 const fisrt = document.querySelector('.first'),
       second = document.querySelector('.second'),
       squares = [...document.querySelectorAll('.square')],
       resetBtn = document.querySelector('.btn-reset');
 
+const line1 = [squares[0],squares[1],squares[2]],
+      line2 = [squares[3],squares[4],squares[5]],
+      line3 = [squares[6],squares[7],squares[8]],
+      line4 = [squares[0],squares[3],squares[6]],
+      line5 = [squares[1],squares[4],squares[7]],
+      line6 = [squares[2],squares[5],squares[8]],
+      line7 = [squares[0],squares[4],squares[8]],
+      line8 = [squares[2],squares[4],squares[6]];
+      
 
 squares.forEach(square => {
   square.addEventListener('click', e => {
 
+    if (turnFirst == true) {
+      square.classList.add('circle');
+      square.classList.add('is-clicked');
+    } else if (turnFirst == false) {
+      square.classList.add('cross');
+      square.classList.add('is-clicked');
+    }
+
     turnFirst = !turnFirst;
-    turnFirst ? square.classList.add('circle') : square.classList.add('cross');
-    square.classList.add('is-clicked');
+
 
   });
 });
