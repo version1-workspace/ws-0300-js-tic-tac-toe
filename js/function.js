536516bdf2b462a12f51306d5ae5de67a82d3f61
Toma Otani <otani.toma@gmail.com>
2020-05-18 11:32:19 -0700

    refactoring

--- js/function.js
+++ js/function.js
@@ -1,9 +1,10 @@
 "use strict";
 
 // 状態管理
-let turnFirst = true;
-let endGame = false;
-let isDraw = false;
+let turnFirst = true,
+    endGame = false,
+    isDraw = false,
+    times = 0;
 
 const fisrt = document.querySelector('.turn-first'),
       second = document.querySelector('.turn-second'),
@@ -23,6 +24,7 @@ const lines = [
   [squares[2],squares[4],squares[6]]
 ];
 
+// 手番を表示する
 function toggleTurnIndicator() {
   if (turnFirst) {
     fisrt.classList.add('is-active');
@@ -34,35 +36,53 @@ function toggleTurnIndicator() {
 }
 
 // 1行・1列でも全て○がつけばtrueを返す
-function judgeWinner(className) {
-  return lines.some(function(line) {
-      return line.every(function(square) {
-        return square.classList.contains(className);
+function judgeWinner(player) {
+  if (player) {
+    const judgeAllLines = lines.some(function(line) {
+      const judgeLine = line.every(function(square) {
+        return square.classList.contains('circle');
       });
+      judgeLine ? endGame = true : endGame = false;
+      endGame ? stopGame() : console.log();
+      return judgeLine;
     });
+    return judgeAllLines;
+  } else {
+    const judgeAllLines = lines.some(function(line) {
+      const judgeLine = line.every(function(square) {
+        return square.classList.contains('cross');
+      });
+      judgeLine ? endGame = true : endGame = false;
+      endGame ? stopGame() : console.log();
+      return judgeLine;
+    });
+    return judgeAllLines;
+  }
 }
 
 // 勝ちが出たら勝者を表示し、すべてのマスをクリック不可にする
-function judgeEndGame() {
-  if (endGame) {
-    squares.forEach(square => {
-      square.classList.add('is-clicked');
-    });
-    statusWinner.textContent = `${turnFirst ? "○" : "×"}`;
-  }
-
-  // 引き分けを判定する
-  const judgeAllLines = lines.every(function(line) {
-    const judgeLine = line.every(function(square) {
-      return square.classList.contains('circle') || square.classList.contains('cross');
-    });
-    judgeLine ? isDraw = true : isDraw = false;
-    return judgeLine;
+function stopGame() {
+  squares.forEach(square => {
+    square.classList.add('is-clicked');
   });
-  if (isDraw) {
-    statusWinner.textContent = 'Draw';
+  statusWinner.textContent = `${turnFirst ? "○" : "×"}`;
+}
+
+// 9手目で引き分けを判定する
+function judgeDraw() {
+  if (times === 9) {
+    lines.every(function(line) {
+      const judgeLine = line.every(function(square) {
+        return square.classList.contains('circle') || square.classList.contains('cross');
+      });
+      judgeLine ? isDraw = true : isDraw = false;
+      return judgeLine;
+    });
+    if (isDraw) {
+      statusWinner.textContent = 'Draw';
+    }
+    return judgeDraw;
   }
-  return judgeAllLines;
 }
 
 // クリックの度に○または×をマスに追加し、勝敗を判定
@@ -71,8 +91,10 @@ squares.forEach(square => {
     const hand = turnFirst ? 'circle' : 'cross'
     square.classList.add(hand);
     square.classList.add('is-clicked');
+    times = times + 1;
+    console.log(times);
+    judgeDraw();
     judgeWinner(turnFirst);
-    judgeEndGame();
     turnFirst = !turnFirst;
     toggleTurnIndicator();
   });
